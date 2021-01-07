5e84086c15442182d18d4803ce1fffd3fc59516b
Nerya Aberdam <nerya.aberdam@live.biu.ac.il>
2021-01-07 14:56:54 +0200

    make kernel

---
+++ out/wwwroot/complexQueries.js
@@ -0,0 +1,425 @@
+﻿
+
+
+function onChangeColumn(event) {
+    if (event.target.value == 'Gdp') {
+        document.getElementById('orderBy').style.display = 'none';
+        document.getElementById('Cumulative').label = 'choose again!';
+        document.getElementById('ThisDay').style.display = 'none';
+        document.getElementById('ThisDay').label = 'choose again!';
+        document.getElementById('untilToday').style.display = 'block';
+        document.getElementById('untilToday').label = 'until today';
+        document.getElementById('dateDownQuery').style.display = 'none';
+        document.getElementById('Avg').style.display = 'none';
+        document.getElementById('Avg').label = 'choose again!';
+        document.getElementById('Div').style.display = 'none';
+        document.getElementById('Div').label = 'choose again!';
+    } else {
+        document.getElementById('ThisDay').label = 'on date';
+        document.getElementById('Cumulative').label = 'until date';
+        document.getElementById('Cumulative').style.display = 'block';
+        document.getElementById('ThisDay').style.display = 'block';
+        document.getElementById('untilToday').label = 'choose again!';
+        document.getElementById('untilToday').style.display = 'none';
+        document.getElementById('dateDownQuery').style.display = 'block';
+        if (sickOrDeath == 'sick') {
+            document.getElementById('Avg').style.display = 'block';
+            document.getElementById('Avg').label = 'each day in avg on the week of';
+            document.getElementById('Div').style.display = 'block';
+            document.getElementById('Div').label = 'each day relative to last week in avg on the week of';
+        }
+
+    }
+}
+
+
+function onsubmitComplex(event) {
+    event.preventDefault();
+    let date = event.target.elements.date.value;
+    //let descOrAsc = event.target.elements.descOrAsc.value;
+    let convertedDate = "" + date[8] + date[9] + "/" + date[5] + date[6] + "/" + date[0] + date[1] + date[2] + date[3];
+    let orderBy = event.target.elements.orderBy.value;
+    let column = event.target.elements.column.value;
+    let descOrAsc = event.target.elements.descOrAsc.value;
+    if (event.target.elements.count.value == 'PerMillion') {
+        if (column == 'PopulationGrowth') { // working
+            popGrowthPerMillion(convertedDate, descOrAsc, event.target.elements.numberOfYears.value, orderBy);
+        }else if (column == 'Density') {
+            densityPerMillion(convertedDate, descOrAsc, orderBy);
+        } else if (column == 'Gdp') { // working
+            gdpPerMillion(convertedDate, descOrAsc, orderBy);
+        }
+    } else if (event.target.elements.count.value == 'TotalNumber') {
+        if (column == 'PopulationGrowth') { // working
+            popGrowthTotal(convertedDate, descOrAsc, event.target.elements.numberOfYears.value, orderBy);
+        } else if (column == 'Density') {
+            densityTotal(convertedDate, descOrAsc, orderBy);
+        } else if (column == 'Gdp') { // working
+            gdpTotal(convertedDate, descOrAsc, orderBy);
+        }
+    }  
+}
+
+function onsubmitAvg(event) {
+     // from SpecificCountryAndDateAvgSick
+    event.preventDefault();
+    let date = event.target.elements.dateDownQuery.value;
+    let convertedDate = "" + date[8] + date[9] + "/" + date[5] + date[6] + "/" + date[0] + date[1] + date[2] + date[3];
+    let descOrAsc = event.target.elements.descOrAscDownQuery.value;
+
+    if (descOrAsc == 'desc') {
+        url = "/api/SpecificCountryAndDateAvgSick/?divOrAvg=Avg&date=" + convertedDate + "&desc=true";
+    } else {
+        url = "/api/SpecificCountryAndDateAvgSick/?divOrAvg=Avg&date=" + convertedDate + "&desc=false";
+    }
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function (data) {
+            deleteTable();
+            let table = document.querySelector("table");
+            //sorttable.makeSortable(table)
+            generateTableHead(table, Object.keys(data[0]));
+            generateTable(table, data);
+        },
+        error: function () {
+            alert("sorry, we don't have this data");
+        }
+    });
+
+}
+
+
+function onsubmitAvgDiv(event) {
+    // from SpecificCountryAndDateAvgSick
+    event.preventDefault();
+    let date = event.target.elements.dateDownQuery.value;
+    let convertedDate = "" + date[8] + date[9] + "/" + date[5] + date[6] + "/" + date[0] + date[1] + date[2] + date[3];
+    let descOrAsc = event.target.elements.descOrAscDownQuery.value;
+
+    if (descOrAsc == 'desc') {
+        url = "/api/SpecificCountryAndDateAvgSick/?divOrAvg=Div&date=" + convertedDate + "&desc=true";
+    } else {
+        url = "/api/SpecificCountryAndDateAvgSick/?divOrAvg=Div&date=" + convertedDate + "&desc=false";
+    }
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function (data) {
+            deleteTable();
+            let table = document.querySelector("table");
+            //sorttable.makeSortable(table)
+            generateTableHead(table, Object.keys(data[0]));
+            generateTable(table, data);
+        },
+        error: function () {
+            alert("sorry, we don't have this data");
+        }
+    });
+
+}
+
+function gdpTotal(date, descOrAsc, orderBy) { //working
+    // from CountrySickDeathsAndGdpByGdpController
+
+    if (descOrAsc == 'desc') {
+        url = "/api/CountrySickDeathsAndGdpByGdp?gdpSickOrDeaths=" + orderBy + "&date=" + date + "&desc=true";
+    } else {
+        url = "/api/CountrySickDeathsAndGdpByGdp?gdpSickOrDeaths=" + orderBy + "&date=" + date + "&desc=false";
+    }
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function (data) {
+            deleteTable();
+            let table = document.querySelector("table");
+            generateTableHead(table, Object.keys(data[0]));
+            generateTable(table, data);
+        },
+        error: function () {
+            alert("sorry, we don't have this data");
+        }
+    });
+}
+
+function gdpPerMillion(date, descOrAsc, orderBy) { // working
+    // from CountryDeathsSickPerMillionWithGdpController
+
+    if (descOrAsc == 'desc') {
+        url = "/api/CountryDeathsSickPerMillionWithGdp?deathsGdpSick=" + orderBy + "&date=" + date + "&desc=true";
+    } else {
+        url = "/api/CountryDeathsSickPerMillionWithGdp?deathsGdpSick=" + orderBy + "&date=" + date + "&desc=false";
+    }
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function (data) {
+            deleteTable();
+            let table = document.querySelector("table");
+            //sorttable.makeSortable(table)
+            generateTableHead(table, Object.keys(data[0]));
+            generateTable(table, data);
+        },
+        error: function () {
+            alert("sorry, we don't have this data");
+        }
+    });
+}
+
+function popGrowthPerMillion(date, descOrAsc, numYears, orderBy) { // working
+    // from CountrySickAndDeathsPerMillionAndGrowthController
+
+    if (descOrAsc == 'desc') {
+        url = "/api/CountrySickAndDeathsPerMillionAndGrowth?date=" + date + "&orderBySickDeathGrowth=" + orderBy + "&numYears=" + numYears+ "&desc=true";
+    } else {
+        url = "/api/CountrySickAndDeathsPerMillionAndGrowth?date=" + date + "&orderBySickDeathGrowth=" + orderBy + "&numYears=" + numYears + "&desc=false";
+    }
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function (data) {
+            deleteTable();
+            let table = document.querySelector("table");
+            //sorttable.makeSortable(table)
+            generateTableHead(table, Object.keys(data[0]));
+            generateTable(table, data);
+        },
+        error: function () {
+            alert("sorry, we don't have this data");
+        }
+    });
+}
+
+function popGrowthTotal(date, descOrAsc, numYears, orderBy) {
+    // from CountrySickAndDeathsAndGrowthController
+    if (descOrAsc == 'desc') {
+        url = "/api/CountrySickAndDeathsAndGrowth?date=" + date + "&orderBySickDeathGrowth=" + orderBy + "&numYears=" + numYears + "&desc=true";
+    } else {
+        url = "/api/CountrySickAndDeathsAndGrowth?date=" + date + "&orderBySickDeathGrowth=" + orderBy + "&numYears=" + numYears + "&desc=false";
+    }
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function (data) {
+            deleteTable();
+            let table = document.querySelector("table");
+            generateTableHead(table, Object.keys(data[0]));
+            generateTable(table, data);
+        },
+        error: function () {
+            alert("sorry, we don't have this data");
+        }
+    });
+}
+
+function densityTotal(date, descOrAsc, orderBy) {
+    // from CountriesDeathsVsDensity2020Controller
+
+    if (descOrAsc == 'desc') {
+        url = "/api/CountriesDeathsVsDensity2020?densityOrDeath=" + orderBy + "&date=" + date + "&desc=true";
+    } else {
+        url = "/api/CountriesDeathsVsDensity2020?densityOrDeath=" + orderBy + "&date=" + date + "&desc=false";
+    }
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function (data) {
+            deleteTable();
+            let table = document.querySelector("table");
+            generateTableHead(table, Object.keys(data[0]));
+            generateTable(table, data);
+        },
+        error: function () {
+            alert("sorry, we don't have this data");
+        }
+    });
+}
+
+function densityPerMillion(date, descOrAsc, orderBy) {
+    // from CountriesDeathsVsDensity2020PerMillionController
+    if (descOrAsc == 'desc') {
+        url = "/api/CountriesDeathsVsDensity2020PerMillion?densityOrDeath=" + orderBy + "&date=" + date + "&desc=true";
+    } else {
+        url = "/api/CountriesDeathsVsDensity2020PerMillion?densityOrDeath=" + orderBy + "&date=" + date + "&desc=false";
+    }
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function (data) {
+            deleteTable();
+            let table = document.querySelector("table");
+            generateTableHead(table, Object.keys(data[0]));
+            generateTable(table, data);
+        },
+        error: function () {
+            alert("sorry, we don't have this data");
+        }
+    });
+}
+
+
+
+//perMillionOrderByDensity(date, descOrAsc, event.target.elements.numberOfYears.value) {
+//    let url;
+//    if (descOrAsc == 'desc') {
+//        url = "/api/CountryDeathsSickPerMillionWithGdp?deathsGdpSick=" + orderBy + "&date=" + date + "&desc=true";
+//    } else {
+//        url = "/api/CountryDeathsSickPerMillionWithGdp?deathsGdpSick=" + orderBy + "&date=" + date;
+//    }
+//    $.ajax({
+//        url: url,
+//        type: "GET",
+//        success: function (data) {
+//            deleteTable();
+//            let table = document.querySelector("table");
+//            generateTableHead(table, Object.keys(data[0]));
+//            generateTable(table, data);
+//        },
+//        error: function () {
+//            alert("sorry, we don't have this data");
+//        }
+//    });
+//}
+
+
+function perMillionOrderBySickGdpDeath(date, orderBy, descOrAsc) {
+    let url;
+    if (descOrAsc == 'desc') {
+        url = "/api/CountryDeathsSickPerMillionWithGdp?deathsGdpSick=" + orderBy + "&date=" + date + "&desc=true";
+    } else {
+        url = "/api/CountryDeathsSickPerMillionWithGdp?deathsGdpSick=" + orderBy + "&date=" + date;
+    }
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function (data) {
+            deleteTable();
+            let table = document.querySelector("table");
+            generateTableHead(table, Object.keys(data[0]));
+            generateTable(table, data);
+        },
+        error: function () {
+            alert("sorry, we don't have this data");
+        }
+    });
+}
+
+
+
+
+function orderByDensity(date, descOrAsc, numberOfYears) {
+    let url;
+    if (descOrAsc == 'desc') {
+        url = "/api/CountrySickDeathsAndGdpByGdp?date=" + date + "&desc=true";
+    } else {
+        url = "/api/CountrySickDeathsAndGdpByGdp?date=" + date;
+    }
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function (data) {
+            deleteTable();
+            let table = document.querySelector("table");
+            generateTableHead(table, Object.keys(data[0]));
+            generateTable(table, data);
+        },
+        error: function () {
+            alert("sorry, we don't have this data");
+        }
+    });
+}
+
+
+function orderByGdp(date, descOrAsc) {
+    let url;
+    if (descOrAsc == 'desc') {
+        url = "/api/CountrySickDeathsAndGdpByGdp?date=" + date + "&desc=true";
+    } else {
+        url = "/api/CountrySickDeathsAndGdpByGdp?date=" + date;
+    }
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function (data) {
+            deleteTable();
+            let table = document.querySelector("table");
+            generateTableHead(table, Object.keys(data[0]));
+            generateTable(table, data);
+        },
+        error: function () {
+            alert("sorry, we don't have this data");
+        }
+    });
+}
+
+function onSubmitNevigate(event) {
+    window.location.href = "index.html";
+}
+
+function checkOrderBy(orderBy) {
+    if (orderBy == 'density') {
+        document.getElementById('numberOfYears').style.display = 'block';
+        document.getElementById('labelForDensity').style.display = 'block';
+    } else {
+        document.getElementById('numberOfYears').style.display = 'none';
+        document.getElementById('labelForDensity').style.display = 'none';
+    }
+}
+
+function checkColumn(newCol) {
+    if (newCol == 'Density') {
+        document.getElementById('DensityOrder').style.display = 'block';
+        document.getElementById('DensityOrder').label = 'density today';
+        document.getElementById('PopulationGrowthOrder').style.display = 'none';
+        document.getElementById('PopulationGrowthOrder').label = 'choose again!';
+        document.getElementById('GdpOrder').style.display = 'none';
+        document.getElementById('GdpOrder').label = 'choose again!';
+        document.getElementById('numberOfYears').style.display = 'none';
+        document.getElementById('labelForDensity').style.display = 'none';
+    } else if (newCol == 'PopulationGrowth') {
+        document.getElementById('PopulationGrowthOrder').style.display = 'block';
+        document.getElementById('PopulationGrowthOrder').label = 'population growth';
+        document.getElementById('numberOfYears').style.display = 'block';
+        document.getElementById('labelForDensity').style.display = 'block';
+        document.getElementById('GdpOrder').style.display = 'none';
+        document.getElementById('DensityOrder').style.display = 'none';
+        document.getElementById('GdpOrder').label = 'choose again!';
+        document.getElementById('DensityOrder').label = 'choose again!';
+    } else if (newCol == 'Gdp') {
+        document.getElementById('GdpOrder').style.display = 'block';
+        document.getElementById('GdpOrder').label = 'gdp';
+        document.getElementById('DensityOrder').style.display = 'none';
+        document.getElementById('PopulationGrowthOrder').style.display = 'none';
+        document.getElementById('DensityOrder').label = 'choose again!';
+        document.getElementById('PopulationGrowthOrder').label = 'choose again!';
+        document.getElementById('numberOfYears').style.display = 'none';
+        document.getElementById('labelForDensity').style.display = 'none';
+    }
+}
+
+function deleteTable() {
+    var Table = document.getElementById("results");
+    Table.innerHTML = "";
+}
+
+function generateTableHead(table, data) {
+    let thead = table.createTHead();
+    let row = thead.insertRow();
+    for (let key of data) {
+        let th = document.createElement("th");
+        let text = document.createTextNode(key);
+        th.appendChild(text);
+        row.appendChild(th);
+    }
+}
+
+function generateTable(table, data) {
+    for (let element of data) {
+        let row = table.insertRow();
+        for (key in element) {
+            let cell = row.insertCell();
+            let text = document.createTextNode(element[key]);
+            cell.appendChild(text);
+        }
+    }
+}
---
+++ out/wwwroot/index.js
@@ -0,0 +1,197 @@
+﻿let marker;
+let map;
+let infos;
+let sickOrDeath = '';
+
+//// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    const uluru = { lat: -25.344, lng: 131.036 };
+    // The map, centered at Uluru
+    map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: uluru,
+    });
+    // The marker, positioned at Uluru
+    marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+    });
+    //google.maps.event.addListener(map, 'click', function (event) {
+    //    alert('Lat: ' + event.latLng.lat() + ' and Longitude is: ' + event.latLng.lng());
+    //});
+}
+
+function onSubmitNevigate(event) {
+    window.location.href = "complexQueries.html";
+}
+
+
+function addMarker(MyLatLong, country, number, state, date, string = ' on ') {
+    //let contentString;
+    let infoWindow = new google.maps.InfoWindow();
+    let contentString;
+    let sickOrDead;
+    if (state == 'Sick') {
+        sickOrDead = 'new cases';
+    } else if (state == 'Deaths') {
+        sickOrDead = 'people who died';
+    }
+    //pop up window marker
+    if (typeof (date) != "undefined") {
+        contentString = "<h6>" + country + "</h6>" + "</br> <p> Number of " + sickOrDead + string + date + " is </p> <h6>" + number + "</h6>";
+    } else {
+        contentString = "<h6>" + country + "</h6>" + "</br> <p> Number of " + sickOrDead + " until today is </p>  <h6>" + number + "</h6>";
+    }
+    if (typeof(infos) != 'undefined') {
+        infos.close();
+    }
+    infoWindow.setContent("<div style = 'width:200px;min-height:40px'>" + contentString + "</div>");
+    infoWindow.open(map, marker);
+    infos = infoWindow;
+    marker.setPosition(MyLatLong);
+    map.setCenter(MyLatLong);
+}
+
+function onSubmitSpecificDate(event) {
+    event.preventDefault();
+    date = event.target.elements.dateUpQuery.value;
+    const url = "/api/CountriesSickOrDeathsThisDay?sickOrDeath=" + event.target.elements.state.value + "&dateReported=" + date;
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function (data) {
+            let number = data[0]['New Cases'];
+            let country = data[0].Country
+            var latLong = getLatLong(country);
+            addMarker(latLong, country, number, event.target.elements.state.value,date);
+
+        },
+        error: function (request) {
+            alert("sorry, we don't have this data");
+        }
+    });
+}
+
+function getLatLong(country) {
+    if (country == "all over the world") {
+        country = "Israel";
+    }
+    const url = "/api/LngLtdOfCountry?country=" + country;
+    var request = new XMLHttpRequest();
+    request.open('GET', url, false);  // `false` makes the request synchronous
+    request.send(null);
+    let lat = request.responseText.split('latitude":')[1].split(",")[0];
+    let long = request.responseText.split('longitude":')[1].split(",")[0].split("}")[0];
+    return new google.maps.LatLng(parseFloat(lat), parseFloat(long));
+}
+
+function onChangeCountry(event) {
+    if (event.target.value == 'allOver') {
+        document.getElementById('Cumulative').style.display = 'none';
+        document.getElementById('Cumulative').label = 'choose again!';
+        document.getElementById('ThisDay').style.display = 'none';
+        document.getElementById('ThisDay').label = 'choose again!';
+        document.getElementById('untilToday').style.display = 'block';
+        document.getElementById('untilToday').label = 'until today';
+        document.getElementById('dateDownQuery').style.display = 'none';
+        document.getElementById('Avg').style.display = 'none';
+        document.getElementById('Avg').label = 'choose again!';
+        document.getElementById('Div').style.display = 'none';
+        document.getElementById('Div').label = 'choose again!';
+    } else {
+        document.getElementById('ThisDay').label = 'on date';
+        document.getElementById('Cumulative').label = 'until date';
+        document.getElementById('Cumulative').style.display = 'block';
+        document.getElementById('ThisDay').style.display = 'block';
+        document.getElementById('untilToday').label = 'choose again!';
+        document.getElementById('untilToday').style.display = 'none';
+        document.getElementById('dateDownQuery').style.display = 'block';
+        if (sickOrDeath == 'sick') {
+            document.getElementById('Avg').style.display = 'block';
+            document.getElementById('Avg').label = 'each day in avg on the week of';
+            document.getElementById('Div').style.display = 'block';
+            document.getElementById('Div').label = 'each day relative to last week in avg on the week of';
+        }
+
+    }
+}
+
+function onChangeDate(event) {
+    if (event.target.value != 'untilToday') {
+        document.getElementById('dateDownQuery').style.display = 'block';
+    } else {
+        document.getElementById('dateDownQuery').style.display = 'none';
+    }
+}
+
+function onChangeState(event) {
+    if (event.target.value == 'Sick') {
+        sickOrDeath = 'sick';
+        document.getElementById('Avg').style.display = 'block';
+        document.getElementById('Avg').label = 'each day in avg on the week of';
+        document.getElementById('Div').style.display = 'block';
+        document.getElementById('Div').label = 'each day relative to last week in avg on the week of';
+        document.getElementById('dateDownQuery').style.display = 'none';
+        //document.getElementById('dateDownQuery').style.display = 'block';
+       
+    } else { // deaths
+        sickOrDeath = 'death';
+        document.getElementById('Avg').style.display = 'none';
+        document.getElementById('Avg').label = 'choose again!';
+        document.getElementById('dateDownQuery').style.display = 'none';
+        //document.getElementById('dateDownQuery').style.display = 'none';
+        document.getElementById('Div').style.display = 'none';
+        document.getElementById('Div').label = 'choose again!';
+    }
+}
+
+function onSubmitSpecificCountrySpecificDateDeathOrSick(event) {
+    event.preventDefault();
+    untilOrThisDate = event.target.elements.untilOrThisDate.value;
+    let date;
+    let url;
+    let country = event.target.elements.country.value;
+    if (untilOrThisDate == 'Avg' || untilOrThisDate == 'Div') {
+        date = event.target.elements.dateDownQuery.value;
+        let convertedDate = "" + date[8] + date[9] + "/" + date[5] + date[6] + "/" + date[0] + date[1] + date[2] + date[3];
+        url = "/api/SpecificCountryAndDateAvgSick/?divOrAvg=" + untilOrThisDate + "&country=" + country + "&date=" + convertedDate;
+    }
+
+    else if (country == "allOver") {
+        url = "/api/OneIntVariable/?deathsOrSick=" + event.target.elements.state.value;
+        country = 'all over the world';
+       
+    } else {
+        date = event.target.elements.dateDownQuery.value;
+        let convertedDate = "" + date[8] + date[9] + "/" + date[5] + date[6] + "/" + date[0] + date[1] + date[2] + date[3];
+        url = "/api/OneIntVariable/?deathsOrSick=" + event.target.elements.untilOrThisDate.value + event.target.elements.state.value + "&date=" + convertedDate + "&country=" + country;
+    }
+    
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function (data) {
+            let number;
+            let stringToSend;
+            if ('sum' in data[0]) {
+                number = data[0].sum;
+                stringToSend = ' on '
+            } else if ('Avg Sick' in data[0]) {
+                number = data[0]['Avg Sick'];
+                if (untilOrThisDate == 'Avg') {
+                    stringToSend = ' in average in each day in the week of '
+                } else if (untilOrThisDate == 'Div') {
+                    stringToSend = ' relative to last week in the week of '
+                }
+                
+            }
+            var latLong = getLatLong(country);
+            addMarker(latLong, country, number, event.target.elements.state.value, date, stringToSend);
+            $("#contact100-form validate-form")[0].reset();
+        },
+        error: function (request) {
+            alert("Sorry, we don't have this data");
+        }
+    });
+}
